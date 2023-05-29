ab147b3ae670722befc894e741d35cd362c50f6f
robinNcode <msmrobin518@gmail.com>
2023-05-29 19:02:38 +0600

    Cache File added

---
+++ Language/hi/Cache.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Cache language settings
+return [
+    'unableToWrite'   => '{0} संचय में लिखने में असमर्थ.',
+    'invalidHandlers' => 'कैश कॉन्फ़िगरेशन में $ValidHandlers की एक सरणी होनी चाहिए।',
+    'noBackup'        => 'कैश कॉन्फ़िगरेशन में एक हैंडलर और बैकअपहैंडलर सेट होना चाहिए।',
+    'handlerNotFound' => 'कैश कॉन्फ़िगरेशन में एक अमान्य हैंडलर या बैकअप हैंडलर निर्दिष्ट किया गया है।',
+];
