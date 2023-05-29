4f903e3a5ba4e243b8deac85ba164c82b4a83481
robinNcode <msmrobin518@gmail.com>
2023-05-29 19:13:08 +0600

    Cast and Cache Language Updated

---
+++ Language/hi/Cast.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Cast language settings
+return [
+    'baseCastMissing'        => '"{0}" वर्ग को "CodeIgniter\Entity\Cast\BaseCast" वर्ग से प्राप्त होना चाहिए।',
+    'invalidCastMethod'      => '"{0}" अमान्य कास्ट विधि है, मान्य विधि है: ["get", "set"]৷',
+    'invalidTimestamp'       => 'टाइप कास्टिंग "timestamp" एक सटीक टाइमस्टैम्प की अपेक्षा करता है৷',
+    'jsonErrorCtrlChar'      => 'अनपेक्षित नियंत्रण वर्ण पाए गए।',
+    'jsonErrorDepth'         => 'स्टैक की अधिकतम गहराई पार हो गई है',
+    'jsonErrorStateMismatch' => 'अंडरफ्लो या मोड बेमेल।',
+    'jsonErrorSyntax'        => 'गलत सिंटेक्स, विकृत JSON',
+    'jsonErrorUnknown'       => 'अज्ञात त्रुटि।',
+    'jsonErrorUtf8'          => 'विकृत UTF-8 वर्ण, संभवतः गलत तरीके से एन्कोड किए गए।',
+];
